c7aa06cd456555bd91add8a47125a48e1a4f7f9c
Mohid01 <mohidb.shahid01@gmail.com>
2026-08-06 00:06:00 +0500

    fix lintr file

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
-{
-  "*.{ts,tsx}": ["npm run lint", "npx tsc --noEmit", "prettier --write"],
-  "*.{js,json,md}": ["npm run lint", "prettier --write"]
-}
+export default {
+  '*.{ts,tsx}': ['npm run lint', 'npx tsc --noEmit', 'prettier --write'],
+  '*.{js,json,md}': ['npm run lint', 'prettier --write'],
+};
