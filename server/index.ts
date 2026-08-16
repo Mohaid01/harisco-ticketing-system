a56aca2055c192be83d0c5edffec765b2e38b588
Mohid01 <mohidb.shahid01@gmail.com>
2026-08-16 14:35:51 +0500

    fix(csp): Updated server to allow Google Fonts in production

--- server/index.ts
+++ server/index.ts
@@ -52,10 +52,10 @@ app.use(
             directives: {
               defaultSrc: ["'self'"],
               scriptSrc: ["'self'"],
-              styleSrc: ["'self'", "'unsafe-inline'"],
+              styleSrc: ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
               imgSrc: ["'self'", 'data:', 'https:'],
               connectSrc: ["'self'", 'ws:', 'wss:'],
-              fontSrc: ["'self'"],
+              fontSrc: ["'self'", 'https://fonts.gstatic.com'],
               objectSrc: ["'none'"],
               mediaSrc: ["'self'"],
               frameSrc: ["'none'"],
